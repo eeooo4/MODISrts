71362234199fa139b486b17ea4c3901c095d8816
Toby <tmaxwel@ucdavis.edu>
2018-05-09 11:01:02 -0700

    update 5.9.2018

--- Modisrts.R
+++ Modisrts.R
@@ -6,13 +6,16 @@ modisProducts() #check out possible products (Beware, the version of Modis matte
 setMRTpath("/Users/Maxwell/MRT/bin/", update=T) #set the path of the modis reprojection tool for the functions to find
 
 #make a date sequence to extract
-start<-paste0(rep(2000:2018, 4) , rep(".04.01",19), rep(".05.01",19), rep(".06.01",19))
-end<-paste0(rep(2000:2018, 4),rep(".04.31",19), rep(".04.31",19), rep(".04.31",19))
+start<-NULL
+end<-NULL
+start<-paste0(rep(2001:2017,1) , rep(".05.01",17))
+end<-paste0(rep(2001:2017,1),rep(".05.31",17))
 start
 end
 #combine into one statement for each desired date so it is easy to loop
+dateseq<-NULL
 dateseq<-paste((start), noquote("\",\""), paste(end), sep = "") 
-
+dateseq
 #extract dateseq for all of oregon, mosaic, and reproject all in one using ModisDownload
   ##note the "MOD13A3" is the modis product
     ###h and v are the modis tiles (can look up online which oncs youll need)
@@ -21,40 +24,43 @@ dateseq<-paste((start), noquote("\",\""), paste(end), sep = "")
           ######utm_zone only needed if using UTM
             ######Datum: projection, pixel size: set what you what, mosaic: default is FALSE, need to set to true to mosaic. If you want there is a separate funciton to do it manually. Same                with proj, which reprojects the mosaicked file to a tif. Versopm: important, seems like 006 has everything I wanted, 005 didnt have much.
 for(i in dateseq){
-ModisDownload("MOD13A3",h=c(8, 9),v=c(4,4),dates=c(i),bands_subset="0 1 0 0 0 0 0 0 0 0 0", proj_type="UTM",proj_params="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",utm_zone=c(10,11),datum="WGS84",pixel_size=1000, mosaic = T, proj=T, version='006')
+ModisDownload("MOD13Q1",h=c(8, 9),v=c(4,4),dates=c(i),bands_subset="0 1 0 0 0 0 0 0 0 0 0", proj_type="UTM",proj_params="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",utm_zone=c(10,11),datum="WGS84",pixel_size=250, mosaic = T, proj=T, version='006')
 }
 
 #Cascade Swath, just a different code using UL and LR to specifiy upper left and lower right bounds in the reprojected coordinate system for the final product. Can act weird...
-ModisDownload("MOD13A3",h=c(8, 9),v=c(4,4),dates=c('2017.04.01','2017.04.31'),bands_subset="0 1 0 0 0 0 0 0 0 0 0", proj_type="UTM",proj_params="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",utm_zone=c(10,11),datum="WGS84",pixel_size=250, mosaic = T, proj=T, UL =c(-130446, 5150545), LR=c(-200000, 4649751), version='006')
+ModisDownload("MOD13Q1",h=c(8, 9),v=c(4,4),dates=c('2001.05.01','2001.05.31'),bands_subset="0 1 0 0 0 0 0 0 0 0 0", proj_type="UTM",proj_params="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",utm_zone=c(10,11),datum="WGS84",pixel_size=250, mosaic = T, proj=T, version='006')
+
+ModisDownload("MOD13Q1",h=c(8, 9),v=c(4,4),dates=c('2017.05.01','2017.05.31'),bands_subset="0 1 0 0 0 0 0 0 0 0 0", proj_type="UTM",proj_params="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0",utm_zone=c(10,11),datum="WGS84",pixel_size=1000, mosaic = T, proj=T, UL =c(-130446, 5150545), LR=c(-200000, 4649751), version='006')
 
 
 library(raster)
 library(rgeos)
 #Show files of some pattern in WD
-listfiles <- list.files(pattern='03-01')
+listfiles <- list.files(pattern='05-01')
 listfiles
-listfiles<-listfiles[1:19]
+listfiles<-listfiles[1:18]
 listfiles
 #Downloaded this separately to crop by
-oregon<-readOGR(dsn="/Users/Maxwell/Documents/geospatial/orcnty2015/", layer = "orcntyline")
+oregon<-readOGR(dsn="/Users/Maxwell/Documents/geospatial/orcnty2015/", layer = "orcntypoly")
 orutm<-spTransform(oregon, crs(raster(listfiles[1])))
 plot(orutm)
 #make a loop to read in all the tif files and crop them to oregon size, then put them all into a rasterstack
 r<-NULL
-eviOR<-NULL
+evimay<-NULL
 for(i in listfiles){
  r<-raster(i)/10000 #divide by 10000 to get into correct units.
  r<-crop(r, orutm)
- eviOR<-stack(r, eviOR)
+ r<-mask(r, orutm)
+ evimay<-stack(r, evimay)
 }
 #changes names to useful ones
-names(eviOR)<-paste0(rep("EVI",19),"March", 2018:2000)
-cellStats(eviOR[[1]]<(-0.2), stat='sum')
-eviOR[eviOR<(-0.2)]<-NA
-plot(eviOR, colNA="BLUE")
+names(evimay)<-paste0(rep("EVI",18),"May", 2017:2000)
+cellStats(evimay[[1]]<(-0.2), stat='sum')
+evimay[evimay<(-0.2)]<-NA
+plot(evimay)
 diffstackOR<-eviOR-eviOR[[19]]
 par(mfrow=c(2,1))
-plot(eviOR[[1]], colNA="lightblue", main = "EVI March, 2018")
+plot(evimay[[1]], colNA="lightblue", main = "EVI March, 2018")
 plot(orutm, add=T)
 plot(diffstackOR[[1]], colNA="lightblue", main = "EVI March, 2018-March 2000")
 plot(orutm, add=T)
@@ -92,10 +98,10 @@ class(x)
 summary(lm(y~x))$coefficients[2]
 
 #actual data calculation
-time<-19:1
+time<-18:1
 
+#March
 eviintercept= calc(eviOR, lm_fun_int)
-
 evip= calc(eviOR, lm_fun_p)
 evip.sig<-evip
 evip.sig<-evip.sig[evip.sig>0.05]<-NA
@@ -104,13 +110,30 @@ plot(evip.sig)
 evislope= calc(eviOR, lm_fun)
 evislopeadj<-evislope
 evislopeadj[evislopeadj>0.1]<-NA
-levelplot(evislopeadj, par.settings=cols2, pretty=T)
+levelplot(evislopeadj, par.settings=cols2)
+
+#May
+eviintmay= calc(evimay, lm_fun_int)
+evipmay= calc(evimay, lm_fun_p)
+evip.sig.may<-evip
+evip.sig.may<-evip.sig.may[evip.sig.may>0.05]<-NA
+plot(evip.sig.may)
+
+evislopemay= calc(evimay, lm_fun)
+evislopeadjmay<-evislopemay
+plot(evislopeadjmay)
+plot(evislopeadjmay[evislopeadjmay>0])
+evislopeadjmay[evislopeadjmay<(-0.02)]<-NA
+evislopeadjmay[evislopeadjmay>0.02]<-NA
+plot(evislopeadjmay)
+levelplot(scale(evislopeadjmay), par.settings=cols2)
 
 
 levelplot(evitrend, contours=T, pretty=T)
+extent(evislopeadj)
 map <- openmap (c( 45.587077, -122.551729), c( 41.989833, -121.104984),type='esri-topo')#provides the map boundary using upper-left and lower-right corners
 plot(map)
-map_utm <- openproj(map, projection =crs(evitrend))
+map_utm <- openproj(map, projection =crs(evislopeadj))
 crs(map_utm)
 plot(map_utm)
 
@@ -124,5 +147,18 @@ levelplot(evitrend, par.settings= cols)
 plot(eviOR[[1]], colNA="lightblue", main = "EVI March, 2018")
 plot(orutm, add=T)
 plot(diffstackOR[[1]], colNA="lightblue", main = "EVI March, 2018-March 2000")
-plotstack<-stack(eviOR[[1]], diffstackOR[[1]])
-levelplot(plotstack, par.settings=cols2, pretty=T)
+plotstack<-stack(scale(evimay[[1]]), scale(evislopeadjmay))
+names(plotstack)<-c("EVI May 2017", "EVI Trend\n ∆EVI/Year")
+levelplot(plotstack, par.settings=cols2, pretty=T, layout=c(2,1))
+par(mfrow =c(1,3))
+plot(eviOR[[1]])
+plot(diffstackOR[[1]])
+
+
+raster::plot(map_utm)
+plot(eviOR[[1]], add=T, alpha=0.7)
+writeRaster(evislopeadj, "evislopeadj.tif")
+
+par(mfrow=c(2,1)
+plot(evislopeadj)
+plot(evislopeadjmay)
